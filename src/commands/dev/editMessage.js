5039b2c40147d4f77b2dd31fd937eb3967e9ad00
Maki <makicoryn@gmail.com>
2024-04-07 14:42:04 +0100

    changed edit message to just the 2 I need

--- src/commands/dev/editMessage.js
+++ src/commands/dev/editMessage.js
@@ -2,37 +2,43 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('edit-message')
-		.setDescription('Edit a bot message')
-		.addStringOption((option) =>
-			option
-				.setName('message_id')
-				.setDescription('The message ID')
-				.setRequired(true),
-		)
-		.addStringOption((option) =>
-			option
-				.setName('message-content')
-				.setDescription('The message content')
-				.setRequired(true),
-		),
+		.setName('edit-messages')
+		.setDescription('Edit a bot message'),
+	// .addStringOption((option) =>
+	// 	option
+	// 		.setName('message_id')
+	// 		.setDescription('The message ID')
+	// 		.setRequired(true),
+	// )
+	// .addStringOption((option) =>
+	// 	option
+	// 		.setName('message-content')
+	// 		.setDescription('The message content')
+	// 		.setRequired(true),
+	// ),
 
 	run: async ({ interaction }) => {
 		if (interaction.user.id !== '442795347849379879') {
 			return interaction.reply('Only the bot owner can run this command.');
 		}
-		const messageID = interaction.options.getString('message_id');
-		const messageContent = interaction.options.getString('message-content');
+		// const messageID = interaction.options.getString('message_id');
+		// const messageContent = interaction.options.getString('message-content');
 
 		try {
-			const message = await interaction.channel.messages.fetch(messageID);
-			await message.edit(messageContent);
-			await interaction.reply(`Edited message with ID ${messageID}`);
-			interaction.deleteReply();
+			const channel = await client.channels.fetch('927897791932542986');
+			const message1 = await channel.messages.fetch('1226503832314581093');
+			const message2 = await channel.messages.fetch('1225913836083740824');
+			await message1.edit(
+				'**Congrats to our newest <@&1193247988269723669> rank, Yohan!**',
+			);
+			await message2.edit(
+				'**Congrats to our newest <@&1193248165676204043> (and <@&1193248143438000158>) rank, touhoe!** _This is a test_',
+			);
+
+			await interaction.reply('Edited messages');
 		} catch (error) {
 			console.error(error);
-			await interaction.reply(`Error editing message with ID ${messageID}`);
-			interaction.deleteReply();
+			await interaction.reply('Error editing messages');
 		}
 	},
 };
