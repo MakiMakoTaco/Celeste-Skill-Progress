0b4a02305870bbe0e5d0156b232c483a6926c385
Zelda <makicoryn@gmail.com>
2025-06-29 23:50:44 +0100

    added timestamp to submissions command

--- src/commands/submissions.js
+++ src/commands/submissions.js
@@ -7,7 +7,7 @@ module.exports = {
 
 	run: async ({ interaction }) => {
 		interaction.reply(
-			'This is a common question we see from newcomers! Submissions can be added to the sheet in two ways.\n\nA submissions script will run twice a day, at 13:00 and 01:00. At this time, all submissions that are correctly formatted will be added to the sheet, and shoutouts will be made! (Roles are not currently given out automatically, but mods try to apply them as they come up. If yours is missed, ping a mod!)\n\nIf your submission is not formatted correctly, or if it is a submission for the Catstare or DLC tabs, it will have to be manually reviewed by a moderator of the sheet, which can take some time. Note, resubmitting will likely not make this faster. Be sure to format your submissions properly, and be patient!',
+			'This is a common question we see from newcomers! Submissions can be added to the sheet in two ways.\n\nA submissions script will run twice a day, at <t:1735693200:t> and <t:1735736400:t>. At this time, all submissions that are correctly formatted will be added to the sheet, and shoutouts will be made! (Roles are not currently given out automatically, but mods try to apply them as they come up. If yours is missed, ping a mod!)\n\nIf your submission is not formatted correctly, or if it is a submission for the Catstare or DLC tabs, it will have to be manually reviewed by a moderator of the sheet, which can take some time. Note, resubmitting will likely not make this faster. Be sure to format your submissions properly, and be patient!',
 		);
 	},
 };
